a78fc935b6035e9408f339fe93e33f804f4f9478
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-05-22 14:34:34 +0000

    Deploy cameo.bio to github.com/biosustain/cameo.git:gh-pages

---
+++ _static/documentation_options.js
@@ -0,0 +1,9 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: '',
+    VERSION: '0.11.3+20.g63cccca',
+    LANGUAGE: 'None',
+    COLLAPSE_INDEX: false,
+    FILE_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt'
+};
